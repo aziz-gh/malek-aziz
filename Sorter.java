fa458682b7b857620e6a584205d79731831d5495
Ryuki17 <sneen2222@gmail.com>
2022-10-10 17:25:06 +0300

    Insertion Sort Method

--- Sorter.java
+++ Sorter.java
@@ -2,5 +2,31 @@ public class Sorter{
     public static void main(String[] args) {
        int x = 1; 
         
+       int[] arr1 = {9,14,3,2,43,11,58,22};    
+        System.out.println("Before Insertion Sort");    
+        for(int i:arr1){    
+            System.out.print(i+" ");    
+        }    
+        System.out.println();    
+            
+        insertionSort(arr1);   
+           
+        System.out.println("After Insertion Sort");    
+        for(int i:arr1){    
+            System.out.print(i+" ");    
+        }
     }
+
+    static void  insertionSort(int[] array){  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        }  
+    }  
 }
